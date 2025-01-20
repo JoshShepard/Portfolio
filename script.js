35179dfe02862018f3dc8f2f616d2f2d1e398b15
Joshua Shepard <joshua_shepard@yahoo.com>
2025-01-20 12:35:38 -0500

    Add comments, tighten up styling, and fixed media query issues project should be done and ready to go live

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Initialize EmailJS with your Public API Key
+// Initialize EmailJS with Public API Key
 emailjs.init("Rfh_ZAZ6NyYG2RtUG");
 
 // Handle form submission
@@ -8,12 +8,17 @@ document.getElementById("contact-form").addEventListener("submit", function (eve
     // Send the form data to EmailJS
     emailjs.sendForm("service_sid3z64", "template_ocziwf4", this)
         .then(function (response) {
-            // Success callback
             console.log("SUCCESS!", response);
+
+            // Inform user form submitted successfully
             alert("Your message has been sent!"); 
+
+            // Reset form for future use/messages
             document.getElementById("contact-form").reset(); 
         }, function (error) {
             console.log("FAILED...", error);
+            
+            // Alert user something has gone wrong
             alert("Oops! Something went wrong. Please try again.");
         });
 });
