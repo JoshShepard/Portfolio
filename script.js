8590a576f9eac7984254b9b410abee1f3ab32d59
JoshShepard <joshua_shepard@yahoo.com>
2025-02-04 21:02:00 -0500

    implemented hamburger menu for mobile viewing

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.getElementById('hamburger');
+    const navList = document.getElementById('nav-list');
+
+    hamburger.addEventListener('click', () => {
+        navList.classList.toggle('active');
+        hamburger.classList.toggle('active');
+    });
+});
+
 // Initialize EmailJS with your Public API Key
 emailjs.init("Rfh_ZAZ6NyYG2RtUG");
 
